2d2e741b006ba0e5709046886ee4ddf3684b4810
levprost <pregibat@gmail.com>
2026-04-17 00:49:43 +0300

    Add task first version

--- base.js
+++ base.js
@@ -1,3 +1,14 @@
 //==========================ADD TASKS section=====================
-const inputTasksElement = document.querySelector('#tasksInput')
+const inputTasksElement = document.querySelector("#taskInput");
 const addTasksButtonElement = document.querySelector('#addBtn')
+const errorMessageElement = document.querySelector('#errorMessage')
+let newTask = ''
+
+addTasksButtonElement.addEventListener('click', () => {
+    if(inputTasksElement.value){
+        newTask = inputTasksElement.value
+    }else{
+        errorMessageElement.innerText = "Error message minimut 1 characters";
+    }
+})
+
