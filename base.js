f1ca46fdcbbec42946ab2f8cad45407206f33cb8
levprost <pregibat@gmail.com>
2026-04-17 01:52:03 +0300

    Add and set task in a list with icons

--- base.js
+++ base.js
@@ -2,13 +2,36 @@
 const inputTasksElement = document.querySelector("#taskInput");
 const addTasksButtonElement = document.querySelector('#addBtn')
 const errorMessageElement = document.querySelector('#errorMessage')
-let newTask = ''
+const tasksListElement = document.querySelector('#taskList')
+let newTask
+let markupDeleteIcon
+let markupUpdateIcon
+const deleteButtonElement = document.querySelector('#deleteBtn')
+
+function addTask(){
+    markupDeleteIcon =
+      '<button id="deleteBtn" class="todo__btn todo__btn--delete"><i class="fa-solid fa-trash-can"></i></button>';
+    markupUpdateIcon =
+      '<button id="updateBtn" class="todo__btn todo__btn--update""><i class="fa-solid fa-circle-check"></i></button>';
+    newTask = `
+        <li class="todo__item">
+        <span class="todo__text">${inputTasksElement.value}</span>
+        <div class="todo__actions">
+            ${markupUpdateIcon}
+            ${markupDeleteIcon}
+        </div>
+        </li>`;
+    tasksListElement.insertAdjacentHTML("afterbegin", newTask);
+}
 
 addTasksButtonElement.addEventListener('click', () => {
     if(inputTasksElement.value){
-        newTask = inputTasksElement.value
+        addTask()
     }else{
         errorMessageElement.innerText = "Error message minimut 1 characters";
     }
 })
+
+//=========================SET TASKS section=========================
+
 
